38e052368860e60138039fb410eff057c5fb6212
char26 <charliealders26@gmail.com>
2025-11-12 15:41:28 -0800

    injest dynamically looks for fields

--- injest.py
+++ injest.py
@@ -1,23 +1,63 @@
+from collections import defaultdict
 import pyshark
 
-cap = pyshark.FileCapture(
-    "/Users/charliealders/SCADA_Data/Modbus/modbus_RTU_6h_1.pcap",
-    display_filter="modbus",
-)
-cap.load_packets()
-print(len(cap))
-print(cap[3])
 
-prev_ts = None
-for packet in cap:
-    modbus = packet.modbus
+def get_all_fields(capture):
+    """
+    Get all fields in the modbus layer for each packet
+    Returns a set of field names
 
-    timestamp = float(packet.sniff_time.timestamp())
-    func_code = int(modbus.func_code)
-    reference_num = int(modbus.reference_num) if hasattr(modbus, "reference_num") else 0
-    word_cnt = int(modbus.word_cnt) if hasattr(modbus, "word_cnt") else 0
+    While it would be faster to hard code the fields for our data, this
+    allows us to easily injest new datasets without changing this code.
 
-    print(timestamp)
-    print(modbus.func_code)
+    Args:
+        capture: pyshark.FileCapture
+            The capture should already be loaded with packets
 
-    break
+    Returns:
+        set: set of field names
+    """
+    all_fields = set()
+    for packet in capture:
+        modbus = packet.modbus
+        all_fields.update(modbus.field_names)
+    return all_fields
+
+
+def load_matrix(capture):
+    """
+    Load the matrix of fields for each packet
+    Returns a dictionary of field names and their values
+
+    Args:
+        capture: pyshark.FileCapture
+            The capture should already be loaded with packets
+
+    Returns:
+        dict: dictionary of field names and their values
+    """
+    all_fields = get_all_fields(capture)
+    dict_matrix = defaultdict(list)
+    for packet in capture:
+        modbus = packet.modbus
+        for field in all_fields:
+            if not hasattr(modbus, field):
+                dict_matrix[field].append(None)
+                continue
+            dict_matrix[field].append(getattr(modbus, field))
+    return dict_matrix
+
+
+def main():
+    capture = pyshark.FileCapture(
+        "/Users/charliealders/SCADA_Data/Modbus/modbus_RTU_6h_1.pcap",
+        display_filter="modbus",
+    )
+    capture.load_packets()
+
+    matrix = load_matrix(capture)
+    print(matrix)
+
+
+if __name__ == "__main__":
+    main()
